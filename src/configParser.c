913386228edf0ccd3d6c0bc75d1f5958bd1b90d2
Jon Schewe <jpschewe@mtu.net>
2012-07-27 18:07:11 -0500

    Initial implementation of config parser

---
+++ src/configParser.c
@@ -0,0 +1,131 @@
+/*
+  Simple configuration file parser.
+
+  Copyright (C) 2012 Jon Schewe
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 3
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+
+
+#include "configParser.h"
+#include <strings.h>
+#include <stdio.h>
+
+static bool isWhitespace(char const data) {
+  return data == ' '
+    || data == '\n'
+    || data == '\r'
+    || data == '\t'
+    || data == '\f'
+    || data == '\v';
+}
+
+
+/**
+ * 
+ */
+char * trimWhitespace(char const * const data) {
+  size_t const len = strlen(data);
+  size_t newLen = 0;
+  char const *begin;
+  char const *end;
+  char *retval = malloc(len);
+
+  if(NULL == retval) {
+    return NULL;
+  }
+
+  retval[0] = '\0';
+  
+  begin = &(data[0]);
+  /* start at -2 as -1 will be \0 */
+  end = &(data[len-2]);
+  
+  while(isWhitespace(*begin) && begin != end) {
+    ++begin;
+  }
+
+  if(begin == end) {
+    return retval;
+  }
+  
+  while(isWhitespace(*end)) {
+    --end;
+  }
+
+  /* Add 1 so we get [being, end] inclusive */
+  newLen = end - begin + 1;
+  
+  memcpy(retval, begin, newLen);
+  retval[newLen] = '\0';
+  
+  return retval;
+}
+
+bool isConfigLine(char const * const line) {
+  size_t const lineLength = strlen(line);
+  if(lineLength == 0) {
+    return false;
+  } else if(line[0] == '#') {
+    return false;
+  } else {
+    /* just check for equals sign */
+    char const *equals = index(line, '=');
+    return equals != NULL;
+  }
+}
+
+bool splitConfigLine(char const * const line,
+                     size_t const keyLength,
+                     char * const key,
+                     size_t const valueLength,
+                     char * const value) {
+  char *equals;
+  size_t actualKeyLength;
+  size_t actualValueLength;
+  
+  if(!isConfigLine(line)) {
+    return false;
+  }
+
+  equals = index(line, '=');
+  if(NULL == equals) {
+    return false;
+  }
+
+  actualKeyLength = equals - line;
+  /*FIXME debug*/
+  printf("Actual key length: %d\n", (int)actualKeyLength);
+  if(actualKeyLength >= keyLength) {
+    /* too big */
+    return false;
+  }
+
+  actualValueLength = strlen(line) - actualKeyLength - 1;
+  /*FIXME debug*/
+  printf("Actual value length: %d\n", (int)actualValueLength);
+  if(actualValueLength >= valueLength) {
+    return false;
+  }
+
+  strncpy(key, line, actualKeyLength);
+  key[actualKeyLength] = '\0';
+  
+  strncpy(value, equals+1, actualValueLength);
+  value[actualKeyLength] = '\0';
+  
+  return true;
+}
